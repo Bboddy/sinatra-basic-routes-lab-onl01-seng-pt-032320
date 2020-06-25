57b4c79869369dbefd85edca5d4f578425766dc2
Bryce B <bryce.boddy@gmail.com>
2020-06-25 06:10:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello, World!"
+  end
+  
+  get '/:name' do 
+    "My name is #{params [:name]}"
+  end
+  
+  get '/:hometown' do 
+    "My name is #{params [:hometown]}"
+  end
+  
+  get '/:favorite-song' do 
+    "My name is #{params [:favorite-song]}"
+  end
 end
